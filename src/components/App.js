0224588c6c13449e61b9ad68f13595fa75d32dc8
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-07-28 10:38:54 +0200

    Recreated the App component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Creating the App component
+const App = () => {
+  return <div className="app-container"></div>;
+};
+
+// Exporting the App component
+export default App;
